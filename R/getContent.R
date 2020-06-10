da521322d423c45011afeeb2a5d04b3ce6cf0bfa
chilampoon <clpoon807@gmail.com>
2020-06-11 02:28:10 +0800

    Add .onLoad and queries of search and species

--- R/getContent.R
+++ R/getContent.R
@@ -6,9 +6,7 @@
 #' Person queries
 #' @param name Person’s first or/and last name
 #' @param id Person's OrcidId or DbId
-#' @param attributes Property for a person
-#' @param host Reactome (https://reactome.org)
-#' @param path path to PERSON
+#' @param attributes Property for a person. Retrieve all available attributes if it is not specified.
 #' @return a list of requested information
 #' @examples
 #' getPerson(name="Robin Haw", attributes=c("displayName", "affiliation"))
@@ -17,8 +15,7 @@
 #' @rdname getPerson
 #' @export 
 
-getPerson <- function(name=NULL, id=NULL, attributes=NULL, 
-                      host="https://reactome.org", path="ContentService/data/person") {
+getPerson <- function(name=NULL, id=NULL, attributes=NULL) {
   # ensure the input
   if (is.null(name) & is.null(id)) stop("Must specify either a name or an ID.")
   
@@ -38,21 +35,21 @@ getPerson <- function(name=NULL, id=NULL, attributes=NULL,
   } 
   
   # retrieve person's information
+  path <- "data/person"
   if (is.null(attributes)) {
     # retrieve all info by default
-    url <- file.path(host, path, id)
+    url <- file.path(getOption("base.address"), path, id)
     all.info <- fromJSON(content(GET(url), 'text'))
     
     # add authored pathways if any
     ap.url <- file.path(url, "authoredPathways")
     authoredPathways <- fromJSON(content(GET(ap.url),'text'))
-    if (length(authoredPathways) != 0) all.info[["authoredPathways"]] <- fromJSON(authoredPathways)
+    if (length(authoredPathways) != 0) all.info[["authoredPathways"]] <- authoredPathways
   } else {
     # retrieve specified properties
     all.info <- list(Id=id)
     for (attribute in attributes) {
-      tmp.url <- file.path(host, path, id, attribute)
-      res=GET(tmp.url)
+      tmp.url <- file.path(getOption("base.address"), path, id, attribute)
       tmp <- content(GET(tmp.url), "parse")
       ifelse(is.character(tmp), 
              all.info[[attribute]] <- tmp, 
@@ -62,20 +59,127 @@ getPerson <- function(name=NULL, id=NULL, attributes=NULL,
   all.info
 }
 
-
-.listPeople <- function(name, host="https://reactome.org", 
-                        path="ContentService/data/people/name", ...) {
+.listPeople <- function(name, ...) {
+  path <- "data/people/name"
   # modify the name and write full URL
   url <- ifelse(grepl("\\s", name),
-                file.path(host, path, tolower(gsub(" ", "%20", name)), "exact"),
-                file.path(host, path, tolower(name)))
+                file.path(getOption("base.address"), path, tolower(gsub(" ", "%20", name)), "exact"),
+                file.path(getOption("base.address"), path, tolower(name)))
   
   res <- tryCatch(GET(url, ...), error = function(e) message("Reactome is not responding"))
+  .checkStatus(res)
   
-  if (status_code(res) != 200) {
-    body <- fromJSON(content(res, "text"))
-    stop(paste0(body[["code"]], " - ", body[["messages"]]))
-  }
   names.df <- fromJSON(content(res, "text"))
   names.df[ ,c(1:5)]
 }
+
+
+
+#' List the whole Reactome search items (species, types, compartments, keywords)
+#' @param what categories of query
+#' @param facet return faceting information or not
+#' @return all available search items
+#' @examples
+#' listSearchItems(what=c("species", "keyword"))
+#' @importFrom httr GET content
+#' @importFrom jsonlite fromJSON
+#' @rdname listSearchItems
+#' @export
+
+listSearchItems <- function(what=c("all", "species", "type", "compartment", "keyword"),
+                            facet=FALSE) {
+  path <- "search/facet"
+  
+  # ensure inputs
+  what <- match.arg(what, several.ok = TRUE)
+  print(what)
+  
+  # retrieve
+  res <- GET(file.path(getOption("base.address"), path))
+  .checkStatus(res)
+  list <- fromJSON(content(res, "text"))
+  
+  # filter
+  ifelse("all" %in% what,
+         select.name <- names(list),
+         select.name <- c(names(list)[1], names(list)[gsub("Facet$", "", names(list)) %in% what]))
+  
+  final.list <- list[select.name]
+  final.list <- lapply(final.list, function(x) if (inherits(x, "list")) {x[["available"]]} else {x})
+  
+  # modify final return
+  if (!facet) {
+    # remove the counts
+    final.list <- final.list[sapply(final.list, function(x) inherits(x, "data.frame"))]
+    final.list <- lapply(final.list, function(x) x$name)
+    names(final.list) <- gsub("Facet$", "", names(final.list))
+  }
+  final.list
+  ## originally I just want to retrieve all available items for c("species", "types", 
+  ## "compartments", "keywords"), a little uncertain whether the faceting information include all terms already 
+  ## since the number of species here is fewer than that from `getSpecies()`
+}
+
+
+
+#' Search query
+#' @param query search term
+#' @param filters filter conditions
+#' @param cluster cluter returned data or not
+#' @param range start row and the nubmer of rows to include
+#' @return a list of information about the search term
+#' @examples
+#' searchQuery("Biological oxidation", 
+#' filters=c(species="Mus musculus", types="", compartments="", keywords=""), 
+#' range=c(0,20))
+#' @importFrom httr GET content
+#' @importFrom jsonlite fromJSON
+#' @rdname searchQuery
+#' @export 
+
+searchQuery <- function(query, filters=c(species="", types="", 
+                                         compartments="", keywords=""), 
+                        cluster=TRUE, range=NULL) {
+  # write full url
+  path <- "search/query"
+  query <- gsub("\\s", "%20", query)
+  url <- file.path(getOption("base.address"), paste0(path, "?query=", query))
+  
+  for (filter in names(filters)) {
+    url <- paste0(url, "&", filter, "=", gsub("\\s", "%20", filters[filter]))
+  }
+  url <- paste0(url, "&cluster=", tolower(cluster))
+  if (!is.null(range)) url <- paste0(url, "&Start%20row=", range[1], "&rows=", range[2])
+  
+  # retrieve
+  res <- GET(url)
+  .checkStatus(res)
+  fromJSON(content(res, "text"))
+}
+
+
+
+#' Species queries
+#' @param main determine whether return main species, which are those have either manually curated or computationally inferred pathways
+#' @return a dataframe of species information
+#' @examples
+#' getSpecies(main=TRUE)
+#' @importFrom httr GET content
+#' @importFrom jsonlite fromJSON
+#' @rdname getSpecies
+#' @export 
+
+getSpecies <- function(main=FALSE) {
+  path <- "data/species"
+  # write the url
+  url <- ifelse(main, 
+                file.path(getOption("base.address"), path, "main"),
+                file.path(getOption("base.address"), path, "all"))
+  
+  res <- GET(url)
+  .checkStatus(res)
+  
+  fromJSON(content(res, "text"))
+}
+
+
--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,48 @@
-#' @importFrom httr GET
+# helper functions
 
-.checkVersion <- function(obj=NULL) {
-  r <- GET('https://reactome.org/ContentService/data/database/version')
-  vs <- content(r, "text")
-  vs
+## The hook function called soon after package loading
+.onLoad <- function(libname, pkgname) {
+  options(base.address = "https://reactome.org/ContentService")
+
+  ### check if any Internal Server Error or something else here? ###
 }
+
+
+
+## Check Reactome's current version
+.checkVersion <- function() {
+  r <- httr::GET(url=file.path(getOption("base.address"), "data/database/version"))
+  version <- httr::content(r, "text")
+  print(paste0("Version ", version))
+}
+
+
+## Check the status of http response
+.checkStatus <- function(res) {
+  if (httr::status_code(res) != 200) {
+    body <- jsonlite::fromJSON(content(res, "text"))
+    stop(paste0(body[["code"]], " - ", body[["messages"]]))
+  }
+}
+
+
+#' Spell check the given search term
+#' @param query A query term
+#' @return a vector of spell-check suggestions
+#' @examples
+#' spellCheck("R-HSA-12355")
+#' spellCheck("oxidition")
+#' @importFrom httr GET content
+#' @importFrom jsonlite fromJSON
+#' @rdname spellCheck
+#' @export
+
+spellCheck <- function(query) {
+  path <- "search/spellcheck"
+  res <- GET(file.path(getOption("base.address"), paste0(path, "?query=", query)))
+  .checkStatus(res)
+  fromJSON(content(res, "text"))
+}
+
+
 
